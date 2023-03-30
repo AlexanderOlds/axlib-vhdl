35d53cbda83bac8a37a93266674ed475f52d683a
AlexanderOlds <alexanderolds@ieee.org>
2023-03-29 23:28:48 -0400

    added uart_tx

--- interface/interface.vhd
+++ interface/interface.vhd
@@ -4,7 +4,7 @@ library ieee;
 
 package interface is
 
-  component uart_rx is
+  component uart_txrx is
     generic (
       clk_freq   : integer   := 100_000_000;
       baudrate   : integer   := 115200;
@@ -25,6 +25,26 @@ package interface is
 
       tx : out   std_logic;
       rx : in    std_logic
+    );
+  end component;
+
+  component uart_tx is
+    generic (
+      clk_freq   : integer   := 100_000_000;
+      baudrate   : integer   := 115200;
+      osrate     : integer   := 16;
+      dwidth     : integer   := 8;
+      parity     : integer   := 0;
+      paritytype : std_logic := '0'
+    );
+    port (
+      clk      : in    std_logic;
+      aresetn  : in    std_logic;
+      txenable : in    std_logic;
+      txdata   : in    std_logic_vector(dwidth - 1 downto 0);
+      txbusy   : out   std_logic;
+
+      tx : out   std_logic
     );
   end component;
 
